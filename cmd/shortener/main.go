edf11ba3988fa8fb34e6c27c323838911b7bbf40
grishagavrin <96040376+grishagavrin@users.noreply.github.com>
2023-03-17 12:07:37 +0300

    iter7 flags first commit

--- cmd/shortener/main.go
+++ cmd/shortener/main.go
@@ -14,6 +14,7 @@ import (
 
 func main() {
 	cfg := config.ConfigENV{}
+	config.SetFlag()
 	serv, exists := cfg.GetEnvValue(config.ServerAddress)
 	if !exists {
 		log.Fatalf("env tag is not created, %s", config.ServerAddress)
--- internal/config/config.go
+++ internal/config/config.go
@@ -1,6 +1,7 @@
 package config
 
 import (
+	"flag"
 	"log"
 	"reflect"
 
@@ -11,16 +12,30 @@ type ConfigENV struct {
 	ServerAddress   string `env:"SERVER_ADDRESS" envDefault:"127.0.0.1:8080"`
 	BaseURL         string `env:"BASE_URL" envDefault:"http://localhost:8080"`
 	FileStoragePath string `env:"FILE_STORAGE_PATH"`
-	// FileStoragePath string `env:"FILE_STORAGE_PATH" envDefault:"/Users/admin/Documents/projects/link-shortener/internal/storage/FileDB.log"`
+	// ServerAddress   string `env:"SERVER_ADDRESS" envDefault:""`
+	// BaseURL         string `env:"BASE_URL" envDefault:"http://localhost:8080"`
 }
 
 const (
-	LENHASH         = 16
 	ServerAddress   = "ServerAddress"
 	BaseURL         = "BaseURL"
 	FileStoragePath = "FileStoragePath"
+	LENHASH         = 16
 	HashSymbols     = "1234567890qwertyuiopasdfghjklzxcvbnm"
 )
+
+var (
+	aFlag string
+	bFlag string
+	fFlag string
+)
+
+func SetFlag() {
+	flag.StringVar(&aFlag, "a", "127.0.0.1:8080", "default host and port")
+	flag.StringVar(&bFlag, "b", "http://localhost:8080", "base url for response query")
+	flag.StringVar(&fFlag, "f", "", "file storage location")
+	flag.Parse()
+}
 
 func (cfg ConfigENV) GetEnvValue(fieldName string) (string, bool) {
 	if err := env.Parse(&cfg); err != nil {
@@ -29,10 +44,29 @@ func (cfg ConfigENV) GetEnvValue(fieldName string) (string, bool) {
 
 	values := reflect.ValueOf(cfg)
 	typesOf := values.Type()
+	var valForEqual string
 	for i := 0; i < values.NumField(); i++ {
-		if typesOf.Field(i).Name == fieldName {
-			return values.Field(i).String(), true
+		if typesOf.Field(i).Name != fieldName {
+			continue
+		} else {
+			valForEqual = values.Field(i).String()
+			if valForEqual != "" {
+				return valForEqual, true
+			}
 		}
+	}
+
+	switch fieldName {
+	case ServerAddress:
+		valForEqual = aFlag
+	case BaseURL:
+		valForEqual = bFlag
+	case FileStoragePath:
+		valForEqual = fFlag
+	}
+
+	if valForEqual != "" {
+		return valForEqual, true
 	}
 
 	return "", false
