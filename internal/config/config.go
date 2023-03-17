3b8e575536a97922410e0d81db399e5039ba6c93
grishagavrin <96040376+grishagavrin@users.noreply.github.com>
2023-03-17 13:01:23 +0300

    iter7 fixies2 after first tests

--- internal/config/config.go
+++ internal/config/config.go
@@ -11,9 +11,7 @@ import (
 type ConfigENV struct {
 	ServerAddress   string `env:"SERVER_ADDRESS" envDefault:"127.0.0.1:8080"`
 	BaseURL         string `env:"BASE_URL" envDefault:"http://localhost:8080"`
-	FileStoragePath string `env:"FILE_STORAGE_PATH"`
-	// ServerAddress   string `env:"SERVER_ADDRESS" envDefault:""`
-	// BaseURL         string `env:"BASE_URL" envDefault:"http://localhost:8080"`
+	FileStoragePath string `env:"FILE_STORAGE_PATH" envDefault:"../../internal/storage/FileDB.log"`
 }
 
 const (
