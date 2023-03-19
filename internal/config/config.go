35c3be1e327ea2d48844c23b2d7d38d50747b5b6
grishagavrin <grigorygavrin@yandex.ru>
2023-03-19 11:21:24 +0300

    Update iter6

--- internal/config/config.go
+++ internal/config/config.go
@@ -11,8 +11,8 @@ import (
 var errUnknownParam = errors.New("unknown env or flag param")
 
 type myConfig struct {
-	BaseURL         string `env:"BASE_URL" envDefault:"http://localhost:8080"`
-	ServerAddress   string `env:"SERVER_ADDRESS" envDefault:"127.0.0.1:8080"`
+	ServerAddress   string `env:"SERVER_ADDRESS" envDefault:":8080"`
+	BaseURL         string `env:"BASE_URL" envDefault:"http://localhost"`
 	FileStoragePath string `env:"FILE_STORAGE_PATH" envDefault:""`
 }
 
@@ -42,8 +42,8 @@ func (c *myConfig) initENV() {
 }
 
 func (c *myConfig) initFlags() {
-	aFlag := flag.String("a", "127.0.0.1:8080", "default host and port")
-	bFlag := flag.String("b", "http://localhost:8080", "base url for response query")
+	aFlag := flag.String("a", ":8080", "default host and port")
+	bFlag := flag.String("b", "http://localhost", "base url for response query")
 	fFlag := flag.String("f", "", "file storage")
 	flag.Parse()
 
@@ -63,26 +63,10 @@ func (c *myConfig) GetCfgValue(env string) (string, error) {
 	case ServerAddress:
 		return c.ServerAddress, nil
 	case BaseURL:
-		return c.BaseURL, nil
+		return c.BaseURL + c.ServerAddress, nil
 	case FileStoragePath:
 		return c.FileStoragePath, nil
 	}
 
 	return "", errUnknownParam
 }
-
-// func (c *MyConfig) GetEnvValue(fieldName string) (string, bool) {
-// 	if err := env.Parse(c); err != nil {
-// 		log.Fatalf("can`t load ENV %+v\n", err)
-// 	}
-
-// 	values := reflect.ValueOf(*c)
-// 	typesOf := values.Type()
-// 	for i := 0; i < values.NumField(); i++ {
-// 		if typesOf.Field(i).Name == fieldName {
-// 			return values.Field(i).String(), true
-// 		}
-// 	}
-
-// 	return "", false
-// }
