cfbdae65af0365f4f1d32d724cabec8a55a9c62f
grishagavrin <grigorygavrin@yandex.ru>
2023-05-27 09:49:22 +0300

    iter13

--- internal/storage/dbstorage/dbsstorage.go
+++ internal/storage/dbstorage/dbsstorage.go
@@ -3,7 +3,6 @@ package dbstorage
 import (
 	"context"
 	"errors"
-	"fmt"
 	"time"
 
 	"github.com/grishagavrin/link-shortener/internal/logger"
@@ -11,7 +10,9 @@ import (
 	"github.com/grishagavrin/link-shortener/internal/user"
 	"github.com/grishagavrin/link-shortener/internal/utils"
 	"github.com/grishagavrin/link-shortener/internal/utils/db"
+	"github.com/jackc/pgerrcode"
 	"github.com/jackc/pgx/v5"
+	"github.com/jackc/pgx/v5/pgconn"
 	"go.uber.org/zap"
 )
 
@@ -20,6 +21,7 @@ type PostgreSQLStorage struct{}
 
 // ErrURLNotFound error by package level
 var ErrURLNotFound = errors.New("url not found")
+var ErrAlreadyHasShort = errors.New("already has short")
 
 func New() (*PostgreSQLStorage, error) {
 	// Check if scheme exist
@@ -29,8 +31,11 @@ func New() (*PostgreSQLStorage, error) {
 		user_id varchar(50),
 		origin  varchar(255) not null,
 		short   varchar(50)  not null,
-		correlation_id varchar(100)
-	);`
+		correlation_id varchar(100)	
+	);
+	CREATE UNIQUE INDEX IF NOT EXISTS short_links_user_id_origin_uindex
+    on public.short_links (user_id, origin);
+	`
 
 	if err := db.Insert(context.Background(), sql); err != nil {
 		return &PostgreSQLStorage{}, err
@@ -93,21 +98,34 @@ func (s *PostgreSQLStorage) SaveLinkDB(userID user.UniqUser, url storage.ShortUR
 		return "", err
 	}
 
-	query := `
+	queryInsert := `
 	INSERT INTO public.short_links (user_id, origin, short) 
 	VALUES (@user_id, @origin, @short);
 	`
+
+	queryGet := `
+	SELECT short FROM public.short_links where user_id=$1 and origin=$2
+	`
+
 	args := pgx.NamedArgs{
 		"user_id": userID,
 		"origin":  url,
 		"short":   key,
 	}
+	dbi, _ := db.Instance()
+	pgErr := &pgconn.PgError{}
 
-	err = db.Insert(ctx, query, args)
-
-	if err != nil {
-		fmt.Println("Unable to insert due to: ", err)
-		return key, err
+	if err == nil {
+		if _, err := dbi.Exec(ctx, queryInsert, args); err != nil {
+			if errors.As(err, &pgErr) {
+				if pgErr.Code == pgerrcode.UniqueViolation {
+					var short storage.URLKey
+					_ = dbi.QueryRow(ctx, queryGet, string(userID), url).Scan(&short)
+					return short, ErrAlreadyHasShort
+				}
+			}
+		}
+		return key, nil
 	}
 
 	return key, nil
@@ -144,7 +162,9 @@ func (s *PostgreSQLStorage) SaveBatch(urls []storage.BatchURL) ([]storage.BatchS
 	// sqlBunchNewRecord for new record in db
 	query := `
 		INSERT INTO public.short_links (user_id, origin, short, correlation_id) 
-		VALUES (@user_id, @origin, @short, @correlation_id);`
+		VALUES (@user_id, @origin, @short, @correlation_id)
+		ON CONFLICT (user_id, origin) DO NOTHING;
+		`
 
 	// Start transaction
 	tx, err := dbi.Begin(ctx)
