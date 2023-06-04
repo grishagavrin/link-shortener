92c4e319758faf70334b7d19a1f4cec44a13a100
grishagavrin <grigorygavrin@yandex.ru>
2023-06-04 19:12:54 +0300

    iter14 finished1

--- internal/config/config.go
+++ internal/config/config.go
@@ -14,7 +14,7 @@ type myConfig struct {
 	ServerAddress   string `env:"SERVER_ADDRESS" envDefault:"127.0.0.1:8080"`
 	BaseURL         string `env:"BASE_URL" envDefault:"http://localhost:8080"`
 	FileStoragePath string `env:"FILE_STORAGE_PATH" envDefault:""`
-	DatabaseDSN     string `env:"DATABASE_DSN" envDefault:"postgresql://postgres:220098@127.0.0.1:5432/golangDB"`
+	DatabaseDSN     string `env:"DATABASE_DSN" envDefault:""`
 }
 
 const (
