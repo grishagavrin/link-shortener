ab0b41a5211902543a4039841662273dea35cbef
grishagavrin <grigorygavrin@yandex.ru>
2023-06-18 20:37:38 +0300

    iter14 fixies18

--- internal/config/config.go
+++ internal/config/config.go
@@ -20,8 +20,8 @@ type myConfig struct {
 	ServerAddress   string `env:"SERVER_ADDRESS" envDefault:"127.0.0.1:8080"`
 	BaseURL         string `env:"BASE_URL" envDefault:"http://localhost:8080"`
 	FileStoragePath string `env:"FILE_STORAGE_PATH" envDefault:""`
-	// DatabaseDSN     string `env:"DATABASE_DSN" envDefault:""`
-	DatabaseDSN string `env:"DATABASE_DSN" envDefault:"postgresql://postgres:220098@127.0.0.1:5432/golangDB"`
+	DatabaseDSN     string `env:"DATABASE_DSN" envDefault:""`
+	// DatabaseDSN string `env:"DATABASE_DSN" envDefault:"postgresql://postgres:220098@127.0.0.1:5432/golangDB"`
 }
 
 var instance *myConfig
