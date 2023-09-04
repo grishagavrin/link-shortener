5212e7c0fff7f3978535efe6ed3f4e1a9d819549
Grisha Gavrin <96040376+grishagavrin@users.noreply.github.com>
2023-09-04 10:43:22 +0300

    iter15 finished

---
+++ internal/utils/bench/utils_test.go
@@ -0,0 +1,17 @@
+package bench
+
+import (
+	"testing"
+
+	"github.com/google/uuid"
+	"github.com/grishagavrin/link-shortener/internal/utils"
+)
+
+func BenchmarkFibo(b *testing.B) {
+	userID := uuid.New().String()
+	testShaUserId := "faa88c146770b06dbe5c90cf9f5dd17b9ac002bcdfa7a403cc59e63fb0f971b1ea9eed9817f426c3397734e43fd22172f3fbf943"
+
+	for i := 0; i < b.N; i++ {
+		_ = utils.Decode(testShaUserId, &userID)
+	}
+}
